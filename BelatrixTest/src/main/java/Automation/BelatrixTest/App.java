fcd57bffa0dbff80c38fd4b264fbd180f7f10850
Christian <christian.jurupe@gmail.com>
2019-05-27 21:45:11 -0500

    Project Created

---
+++ BelatrixTest/src/main/java/Automation/BelatrixTest/App.java
@@ -0,0 +1,13 @@
+package Automation.BelatrixTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
